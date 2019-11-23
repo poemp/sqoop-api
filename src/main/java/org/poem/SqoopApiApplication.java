5fac19776d949a5cc5e25518c313f050c3f830d8
luyanwen <xue_2013@sina.com>
2019-11-23 16:15:32 +0800

    firstadd

---
+++ src/main/java/org/poem/SqoopApiApplication.java
@@ -0,0 +1,13 @@
+package org.poem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqoopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqoopApiApplication.class, args);
+    }
+
+}
